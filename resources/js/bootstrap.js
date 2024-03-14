31e64e4f46ba5ee9941c225c3a5f5dae56fd0f69
AlexanderKira <superair12345@gmail.com>
2024-03-14 13:59:15 +0700

    Installed: node.js imported: bootstrap jquery popper.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,13 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import 'bootstrap';
+import 'jquery';
+import 'popper.js';
+
+window.jQuery = $;
+window.$ = $;
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
